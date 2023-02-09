efb400b29975121168e138df50db847d3e9e8021
VVR <bbr-53@yandex.ru>
2023-02-09 15:26:03 +0300

    Изменен на работе 9.02.23

--- EhoBot.py
+++ EhoBot.py
@@ -1,6 +1,8 @@
 from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
+from aiogram.filters import BaseFilter
 from aiogram.types import Message
+from aiogram.filters import Text
+
 
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
 API_TOKEN: str = '5923154900:AAECR2DEtm_zLqnNzRsXy5Wzjm83RqYizDc'
@@ -9,28 +11,68 @@ API_TOKEN: str = '5923154900:AAECR2DEtm_zLqnNzRsXy5Wzjm83RqYizDc'
 bot: Bot = Bot(token=API_TOKEN)
 dp: Dispatcher = Dispatcher()
 
+# Список с ID администраторов бота. !!!Замените на ваш!!!
+admin_ids: list = [1895911562]
+#
+# # Собственный фильтр, проверяющий юзера на админа
+# class IsAdmin(BaseFilter):
+#     def __init__(self, admin_ids: list) -> None:
+#         # В качестве параметра фильтр принимает список с целыми числами
+#         self.admin_ids = admin_ids
+#
+#     async def __call__(self, message: Message) -> bool:
+#         return message.from_user.id in self.admin_ids
+#
+#
+# # Этот хэндлер будет срабатывать, если апдейт от админа
+# @dp.message(IsAdmin(admin_ids))
+# async def answer_if_admins_update(message: Message):
+#     await message.answer(text='Вы админ')
+#
+#
+# # Этот хэндлер будет срабатывать, если апдейт не от админа
+# @dp.message()
+# async def answer_if_not_admins_update(message: Message):
+#     await message.answer(text='Вы не админ')
 
-# Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands=["start"]))
-async def process_start_command(message: Message):
-    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+# Этот фильтр будет проверять наличие неотрицательных чисел
+# в сообщении от пользователя, и передавать в хэндлер их список
 
 
-# Этот хэндлер будет срабатывать на команду "/help"
-@dp.message(Command(commands=['help']))
-async def process_help_command(message: Message):
-    await message.answer('Напиши мне что-нибудь и в ответ '
-                         'я пришлю тебе твое сообщение')
+class NumbersInMessage(BaseFilter):
+    async def __call__(self, message: Message) -> bool or dict[str, list]:
+        numbers = []
+        # Разрезаем сообщение по пробелам, нормализуем каждую часть, удаляя
+        # лишние знаки препинания и невидимые символы, проверяем на то, что
+        # в таких словах только цифры, приводим к целым числам
+        # и добавляем их в список
+        for word in message.text.split():
+            normalized_word = word.replace('.', '').replace(',', '').strip()
+            if normalized_word.isdigit():
+                numbers.append(int(normalized_word))
+        # Если в списке есть числа - возвращаем список по ключу 'numbers'
+        if numbers:
+            return {'numbers': numbers}
+        return False
+
+# Этот хэндлер будет срабатывать, если сообщение пользователя
+# начинается с фразы "найди числа" и в нем есть числа
+@dp.message(Text(startswith='найди числа', ignore_case=True),
+            NumbersInMessage())
+# Помимо объекта типа Message, принимаем в хэндлер список чисел из фильтра
+async def process_if_numbers(message: Message, numbers: list):
+    await message.answer(
+            text=f'Нашел: {str(", ".join(str(num) for num in numbers))}')
 
 
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
-# кроме команд "/start" и "/help"
-@dp.message()
-async def send_echo(message: Message):
-    await bot.send_message(message.chat.id, message.text)
-    await message.answer('Какой-то текст')
-    # await message.reply(text=message.text)
+# Этот хэндлер будет срабатывать, если сообщение пользователя
+# начинается с фразы "найди числа", но в нем нет чисел
+@dp.message(Text(startswith='найди числа', ignore_case=True))
+async def process_if_not_numbers(message: Message):
+    await message.answer(
+            text='Не нашел что-то :(')
 
 
 if __name__ == '__main__':
     dp.run_polling(bot)
+
--- some_list.py
+++ some_list.py
@@ -10,3 +10,56 @@
 #
 # print(custom_filter(some_list))
 
+# def custom_filter(some_list: list, sum: int=0, criteria: int=83) -> bool:
+#     for elem in some_list:
+#         if type(elem) == int and elem % 7 == 0:
+#                 sum+=elem
+#     return not sum>criteria
+
+# def custom_filter(some_list):
+#     return sum(filter(lambda x: type(x)==int and x%7==0, some_list))<83
+#
+# my_list: list=[]
+# my_list = [7, 14, 28, 32, 32, 56]
+# #my_list = [7, 14, 28, 32, 32, '56']
+#
+# print(custom_filter(my_list))
+
+#counter = test_str.count('t')
+
+# def custom_filter(some_list):
+#     return sum(filter(lambda x: type(x)==int and x%7==0, some_list))<83
+
+# anonimous_filter = lambda x: (x.count('я') + x.count('Я'))>=23
+# #
+# print(anonimous_filter('последняя буква в алфавите Я - !'))
+#
+# print(anonimous_filter('яяяяяяяяяяяяяяяяяяяяяяяя, яяяяяяяяяяяяяяяя и яяяяяяяя тоже!'))
+
+# class MyClass:
+#     def __init__(self) -> None:
+#         pass
+#
+#     def __call__(self) -> str:
+#         return 'Результат вызова экземпляра класса'
+#
+# my_class_1 = MyClass()
+# my_class_2 = MyClass()
+#
+# print(my_class_1())
+# print(my_class_2())
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import BaseFilter
+from aiogram.types import Message
+
+
+
+
+
+
+
+
+
+
+
